674ff1770e35bd9252fd336d25d8550170d0fe1a
Sam Kim <samuelkim523@gmail.com>
2017-11-05 05:11:29 -0500

    PH_Sender now works for OUT, IN, DATA0... But there is an issue with DPDM_Encodegit add PH_Sender.sv

--- PH_Sender.sv
+++ PH_Sender.sv
@@ -11,26 +11,29 @@ module SendPIDtoDPDM_FSM
   always_comb begin
     {shift, ph_sending, cnt_clr, cnt_inc} = 4'b0000; // defaults
 
-    IDLE : begin
-      if (~start) begin
-        nextState = IDLE;
-      end else begin
-        cnt_clr = 1;
-        ph_sending = 1;
+    case (currState)
+      IDLE : begin
+        if (~start) begin
+          nextState = IDLE;
+        end else begin
+          cnt_clr = 1;
+          ph_sending = 1;
+        end
       end
-    end
 
-    SEND : begin
-      if (bit_cnt != 3'd7) begin
-        shift = 1;
-        cnt_inc = 1;
-        ph_sending = 1;
+      SEND : begin
+        if (bit_cnt != 3'd7) begin
+          shift = 1;
+          cnt_inc = 1;
+          ph_sending = 1;
 
-        nextState = SEND;
-      end else begin
-        nextState = IDLE;
+          nextState = SEND;
+        end else begin
+          nextState = IDLE;
+        end
       end
-    end
+    endcase // currState
+
   end
 
   always_ff @(posedge clock, negedge reset_n) begin
@@ -47,7 +50,8 @@ module PH_Sender
   (input  logic        clock, reset_n,
                        send_OUT, send_IN, send_DATA0, send_ACK, send_NAK,
    input  logic  [3:0] endp,  // If we need it for OUT or IN
-   input  logic [63:0] data); // If we need it for DATA0
+   input  logic [63:0] data,  // If we need it for DATA0
+   output logic        DP_out, DM_out, out_done);
 
   // HARD-CODED PIDS
   logic [7:0] out_pid, in_pid, data0_pid, ack_pid, nak_pid;
@@ -63,11 +67,12 @@ module PH_Sender
   logic [6:0] addr;
   assign addr = 7'd5; // addr is always 5
 
-  logic [23:0] crc5_pkt_in;
+  logic [18:0] crc5_pkt_in;
   logic crc5_pkt_ready;
   always_comb begin
     {crc5_pkt_in} = 24'bx; // default
 
+    // NOTE: FINE TO BE COMB, B/C CRC MODULES LOAD INTO REGISTER
     if (send_OUT) begin
       crc5_pkt_in = {endp, addr, out_pid};
     end else if (send_IN) begin
@@ -78,9 +83,9 @@ module PH_Sender
   end // always_comb
 
   /*********************** DATA0 PREPARATION FOR CRC16  ***********************/
-  logic [88:0] crc16_pkt_in;
+  logic [71:0] crc16_pkt_in;
   logic crc16_pkt_ready;
-  assign crc16_pkt_in = {data, git};
+  assign crc16_pkt_in = {data, data0_pid};
   assign crc16_pkt_ready = send_DATA0;
 
   /******************* ACK/NAK PREPARATION FOR DPDM_ENCODER *******************/
@@ -96,7 +101,8 @@ module PH_Sender
   // PISO register to hold the ACK/NAK PID
   logic load, shift, ph_out_bit;
   assign load = (send_ACK || send_NAK);
-  PISO_Register_Right reg (#8) (.D(dpdm_pid), .Q(ph_out_bit),
+  // PISO_Register_Right dut (.*);
+  PISO_Register_Right #(8) pid_reg (.D(dpdm_pid), .Q(ph_out_bit),
                                 .load(load), .shift(shift),
                                 .*);
 
@@ -106,7 +112,7 @@ module PH_Sender
   always_ff @(posedge clock, negedge reset_n) begin
     if(~reset_n) begin
       bit_cnt <= 3'd0;
-    else if (cnt_clr) begin
+    end else if (cnt_clr) begin
       bit_cnt <= 3'd0;
     end else begin
       bit_cnt <= bit_cnt + 1;
@@ -116,7 +122,7 @@ module PH_Sender
   // FSM to send PID to DPDM
   logic start, ph_sending;
   assign start = (send_ACK || send_NAK);
-  SendPIDtoDPDM_FSM fsm (.shift(dpdpm_reg_shift), .*);
+  SendPIDtoDPDM_FSM fsm (.shift(shift), .*);
   //   (input  logic clock, reset_n,
   //    input  logic start,
   //    output logic shift, ph_sending);
@@ -127,29 +133,46 @@ module PH_Sender
   logic bs_ready, // input from BitStuffer
         crc5_out_bit, crc5_valid_out,
         crc16_out_bit, crc16_valid_out; // output to BitStuffer
-  CRC5_Calc   crc5 (.pkt_in(crc5_pkt_in), .pkt_ready(crc5_pkt_ready),
+  CRC5_Encode   crc5 (.pkt_in(crc5_pkt_in), .pkt_ready(crc5_pkt_ready),
                     .out_bit(crc5_out_bit), .crc_valid_out(crc5_valid_out),
                     .*);
-  CRC16_Calc crc16 (.pkt_in(crc16_pkt_in), .pkt_ready(crc16_pkt_ready),
+  // (input  logic clock, reset_n,
+  //               pkt_ready,     // PH ready to send us a packet
+  //               bs_ready,      // BS ready to receive bits
+  //  input  logic [18:0] pkt_in, // orig packet from protocol handler
+  //  output logic out_bit,       // bit going to BS
+  //               crc_valid_out);  // telling BS we are sending bits
+  CRC16_Encode crc16 (.pkt_in(crc16_pkt_in), .pkt_ready(crc16_pkt_ready),
                     .out_bit(crc16_out_bit), .crc_valid_out(crc16_valid_out),
                     .*);
 
   // BITSTUFFER MODULE INSTANTIATION
   logic bs_out_bit, bs_sending;
-  BitStuffer bs (.crc5_valid_out(crc5_valid_out),
+  BS_Encode bs (.crc5_valid_out(crc5_valid_out),
                  .crc5_in_bit(crc5_out_bit),
                  .crc16_valid_out(crc16_valid_out),
-                 .crc16_in_bit(crc16_out_bit), .*);
+                 .crc16_in_bit(crc16_out_bit),
+                 .out_bit(bs_out_bit), .*);
+  // (input  logic clock, reset_n,
+  //               crc5_valid_out, crc5_in_bit,
+  //               crc16_valid_out, crc16_in_bit,                
+  //  output logic out_bit, bs_ready, bs_sending);
 
   // NRZI MODULE INSTANTIATION
   logic nrzi_out_bit, nrzi_sending;
-  NRZI_Encoder nrzi (.in_bit(bs_out_bit), .out_bit(nrzi_out_bit), .*);
+  NRZI_Encode nrzi (.in_bit(bs_out_bit), .out_bit(nrzi_out_bit), .*);
+  // (input logic clock, reset_n,
+  //              in_bit, bs_sending,
+  //  output logic out_bit, nrzi_sending);
 
   // DPDM MODULE INSTANTIATION
-  logic out_DP, out_DM, out_done;
-  DPDM dpdm (.in_bit(nrzi_out_bit),
+  DPDM_Encode dpdm (
              .nrzi_in_bit(nrzi_out_bit), .nrzi_sending(nrzi_sending),
-             .ph_in_bit(ph_out_bit), .ph_sending)ph_sending),
-             .DP(out_DP), .DM(out_DM), .*);
+             .ph_in_bit(ph_out_bit), .ph_sending(ph_sending),
+             .DP(DP_out), .DM(DM_out), .out_done(out_done), .*);
+  // (input  logic clock, reset_n,
+  //               nrzi_in_bit, nrzi_sending,
+  //               ph_in_bit, ph_sending,
+  //  output logic DP, DM, out_done);
 
 endmodule : PH_Sender
