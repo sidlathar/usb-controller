b5df436f1e27223ca54f9f9a7b89feef05a28a48
SL <slathar@andrew.cmu.edu>
2017-11-09 02:58:29 -0500

    assembled all the reciever modules

--- receive/PH_Receiver.sv
+++ receive/PH_Receiver.sv
@@ -64,18 +64,59 @@ endmodule: PH_Receiver_fsm
 
 
 module PH_Receiver
-	(input logic clock, reset_n, rec_start, crc_sending,
-	 input logic crc_valid, ACK_rec, NAK_rec, DATA0_rec,
-	 input logic [63:0] data0,
+	(input logic clock, reset_n,
 	output logic rec_ACK, rec_NAK, rec_DATA0,
-	output logic [63:0] data_rec, data_valid);
+	output logic [63:0] data_rec, data_valid); //TO READ WRITE FSM
 
 
-	 PH_Receiver_fsm fsm(.*);
+	// input logic clock, reset_n,
+	// input logic DP_in, DM_in,
+	// output logic out_bit, dpdm_sending, rec_start, load_data,
+	// output logic ACK_rec, NAK_rec, DATA0_rec);
+	logic DP_in, DM_in, rec_start;
+	logic dpdm_out_bit, dpdm_sending, load_data;
+	logic ACK_rec, NAK_rec, DATA0_rec;
 
-	 assign data_rec = data0;
-	 assign data_valid = crc_valid;
+	DPDM_decode dpdmDecode(.out_bit(dpdm_out_bit),.*);
 
 
+
+	// module NRZI_decoder
+	//   (input logic clock, reset_n,
+	//                in_bit, dpdm_sending, // insted of bs_sending it shuld be DPDM sending?
+	//    output logic out_bit, nrzi_sending);
+	logic nrzi_out_bit, nrzi_sending;
+
+	NRZI_decoder nrziDecode(.in_bit(dpdm_out_bit), .out_bit(nrzi_out_bit), .*);
+
+
+	// // module BitStuffer_decode
+	// //   (input  logic clock, reset_n,
+	// //                 nrzi_sending, in_bit,
+	// //    output logic out_bit, bs_sending);
+
+	logic bs_sending, bs_out_bit;
+
+	BitStuffer_decode bsDecode(.in_bit(nrzi_out_bit), .out_bit(bs_out_bit), .*);
+
+
+	// (input  logic clock, reset_n,
+	//          bs_sending, load_data,     
+	//  input  logic in_bit,
+	//  output logic out_bit, 
+	//          crc_sending, crc_valid,
+	//  output logic [63:0] data0);  
+
+	logic crc_sending, crc_out_bit, crc_valid;
+	logic [63:0] data0;
+
+	CRC16_Decode crc16Decode(.in_bit(bs_out_bit), .out_bit(crc_out_bit), .*);
+
+
+
+	assign data_rec = data0;
+	assign data_valid = crc_valid;
+
+	PH_Receiver_fsm fsm(.*);
 
 endmodule: PH_Receiver // PH_Receiver
